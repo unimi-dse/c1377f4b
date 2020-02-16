3a0d8b311a44f6932dd5d391cac1494b9607348d
ceerbia <mmagnani.marta@gmail.com>
2020-02-16 23:24:14 +0100

    ,

--- R/Score700.R
+++ R/Score700.R
@@ -5,14 +5,13 @@
 #'@return plot
 #'
 #'@export
-#'
 
 Score700 <- function() {
 
   library(ggplot2)
   library(dplyr)
 
-  cwur.data$score <- as.numeric(cwur.data$score)
+  #cwur.data$score <- as.numeric(cwur.data$score)
 
   f3 <- cwur.data %>% select(institution,score,year)%>%filter(score>700)%>%ggplot(aes(x=year,y=score,group=institution,col=factor(institution)))+geom_line()+theme(axis.text.x=element_text(angle=90))+labs(cwur.data, title= "Score trend of institutions with score over 700 across years")
   return(f3)
--- R/ScoreInteractive.R
+++ R/ScoreInteractive.R
@@ -11,7 +11,7 @@ ScoreInteractive <- function(setpoint) {
   library(ggplot2)
   library(dplyr)
 
-  cwur.data$score <- as.numeric(cwur.data$score)
+  #cwur.data$score <- as.numeric(cwur.data$score)
 
   f3 <- cwur.data %>% select(institution,score,year)%>%filter(score>setpoint)%>%ggplot(aes(x=year,y=score,group=institution,col=factor(institution)))+geom_line()+theme(axis.text.x=element_text(angle=90))+labs(cwur.data, title= print(paste("Score trend of institutions with score over", setpoint, "across years")))
   return(f3)
