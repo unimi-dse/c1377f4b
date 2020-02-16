ccbb361e9179387bc99cbc5bdbc23c14f0625eed
ceerbia <mmagnani.marta@gmail.com>
2020-02-16 19:21:14 +0100

    .

--- R/Score700.R
+++ R/Score700.R
@@ -1,20 +1,20 @@
-#'Score700
+#'ScoreInteractive
 #'
-#'@description The function Score700 paragones universities with score>700 from 2012 to 2015.
+#'@description This function takes a numeric argument to plot the scores of the universities over years.
 #'
 #'@return plot
 #'
 #'@export
 
-Score700 <- function() {
+ScoreInteractive <- function(setpoint) {
 
   library(ggplot2)
   library(dplyr)
 
   cwur.data$score <- as.numeric(cwur.data$score)
 
-  f3 <- cwur.data %>% select(institution,score,year)%>%filter(score>700)%>%ggplot(aes(x=year,y=score,group=institution,col=factor(institution)))+geom_line()+theme(axis.text.x=element_text(angle=90))+labs(cwur.data, title='Score trend of institutions with score over 700 across years')
+  f3 <- cwur.data %>% select(institution,score,year)%>%filter(score>setpoint)%>%ggplot(aes(x=year,y=score,group=institution,col=factor(institution)))+geom_line()+theme(axis.text.x=element_text(angle=90))+labs(cwur.data, title= print(paste("Score trend of institutions with score over", setpoint, "across years")))
   return(f3)
-}
+  }
 
 
