c5290fc163802d81a2a84c344d9f73b4ba290e87
ceerbia <mmagnani.marta@gmail.com>
2020-02-16 19:27:05 +0100

    ..

--- R/Score700.R
+++ R/Score700.R
@@ -1,20 +1,18 @@
-#'ScoreInteractive
+#'Score700
 #'
-#'@description This function takes a numeric argument to plot the scores of the universities over years.
+#'@description This function plots the score trend over 700 of institutions across years.
 #'
 #'@return plot
 #'
 #'@export
 
-ScoreInteractive <- function(setpoint) {
+Score700 <- function(setpoint) {
 
   library(ggplot2)
   library(dplyr)
 
   cwur.data$score <- as.numeric(cwur.data$score)
 
-  f3 <- cwur.data %>% select(institution,score,year)%>%filter(score>setpoint)%>%ggplot(aes(x=year,y=score,group=institution,col=factor(institution)))+geom_line()+theme(axis.text.x=element_text(angle=90))+labs(cwur.data, title= print(paste("Score trend of institutions with score over", setpoint, "across years")))
+  f3 <- cwur.data %>% select(institution,score,year)%>%filter(score>700)%>%ggplot(aes(x=year,y=score,group=institution,col=factor(institution)))+geom_line()+theme(axis.text.x=element_text(angle=90))+labs(cwur.data, title= "Score trend of institutions with score over 700 across years")
   return(f3)
-  }
-
-
+}
---
+++ R/ScoreInteractive.R
@@ -0,0 +1,18 @@
+#'ScoreInteractive
+#'
+#'@description This function takes a numeric argument to plot the scores of the universities over years.
+#'
+#'@return plot
+#'
+#'@export
+
+ScoreInteractive <- function(setpoint) {
+
+  library(ggplot2)
+  library(dplyr)
+
+  cwur.data$score <- as.numeric(cwur.data$score)
+
+  f3 <- cwur.data %>% select(institution,score,year)%>%filter(score>setpoint)%>%ggplot(aes(x=year,y=score,group=institution,col=factor(institution)))+geom_line()+theme(axis.text.x=element_text(angle=90))+labs(cwur.data, title= print(paste("Score trend of institutions with score over", setpoint, "across years")))
+  return(f3)
+}
