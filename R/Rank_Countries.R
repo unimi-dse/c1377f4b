df488aacfe0efc7e258ded727378151caa9ed08e
ceerbia <mmagnani.marta@gmail.com>
2020-02-16 18:59:04 +0100

    .

--- R/Rank_Countries.R
+++ R/Rank_Countries.R
@@ -15,12 +15,7 @@ Rank_Countries <- function() {
   RankingCount <- cwur.data %>% group_by(country) %>% summarise(count=n())
 
   #create the ggplot containing
-  f1 <- ggplot(RankingCount, aes(x=reorder(country, -count), y=count, fill=country))
-  + geom_bar(stat="identity")
-  + coord_flip()
-  + theme(legend.position="none")
-  + labs(x="Country",y="Count")
-  + ggtitle("Number of listed universities for each country")
+  f1 <- ggplot(RankingCount, aes(x=reorder(country, -count), y=count, fill=country))+geom_bar(stat="identity")+coord_flip()+ theme(legend.position="none")+ labs(x="Country",y="Count")+ ggtitle("Number of listed universities for each country")
 
   return(f1)
 }
--- R/Score65.R
+++ R/Score65.R
@@ -1,19 +1,19 @@
-#'Score65
+#'Score700
 #'
-#'@description The function Score65 paragones universities with score>65 from 2012 to 2015.
+#'@description The function Score700 paragones universities with score>700 from 2012 to 2015.
 #'
 #'@return plot
 #'
 #'@export
 
-Score65 <- function() {
+Score700 <- function() {
 
   library(ggplot2)
   library(dplyr)
 
   cwur.data$score <- as.numeric(cwur.data$score)
 
-  f3 <- cwur.data %>% select(institution,score,year)%>%filter(score>700)%>%ggplot(aes(x=institution,y=score,group=year,col=factor(year)))+geom_line()+theme(axis.text.x=element_text(angle=90))+labs(cwur.data, title='Insitutions with Score over 700')
+  f3 <- cwur.data %>% select(institution,score,year)%>%filter(score>700)%>%ggplot(aes(x=year,y=score,group=institution,col=factor(institution)))+geom_line()+theme(axis.text.x=element_text(angle=90))+labs(cwur.data, title='Score trend of institutions with score over 700 across years')
   return(f3)
 }
 
